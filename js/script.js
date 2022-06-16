c7e699ea976ef3546024a24eefa3feb472d136dd
Whoan10 <juanvitorioclash@gmail.com>
2022-06-16 10:18:22 -0300

    criei o js

---
+++ js/script.js
@@ -0,0 +1,23 @@
+function start(){
+  bandeiras()
+}
+
+function bandeiras(){
+
+  var flag = document.getElementById('flag')
+
+  var img_flags = []
+  var continentes = [america_latina, europa, oceania, asia]
+  
+  var america_latina = ['Brasil','Chile', 'Peru', 'Argentina', 'Equador', 'Bolívia']
+  var europa = []
+  var oceania = []
+  var asia = []
+
+  var sorteador_continente = ''
+
+  for(var i = 0; i < america_latina.length; i++ ){
+    
+  }
+
+}
